26911e9e1f4d62e57cd72f618eca48ea180dd80e
Julio Z <julioz@Julios-MacBook-Pro.local>
2020-07-07 21:12:52 -0600

    small update

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+
+// local imports
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//tests 
+
+describe('App', () => {
+  test('renders App component', async () => {
+    render(<App />);
+ 
+    expect(screen.queryByText(/Signed in as/)).toBeNull();
+ 
+    expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+  })
 });
